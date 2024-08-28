2d4a43461a02258ccbe49e3fcdb1219f8418863d
MistrrrMedia <music.mistrrr@gmail.com>
2024-08-28 15:22:01 -0500

    Initial Commit

---
+++ svdden tempo/svdden_tempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  svdden_tempoApp.swift
+//  svdden tempo
+//
+//  Created by Nicholas Nguyen on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct svdden_tempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
